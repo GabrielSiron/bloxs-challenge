bcf467ea09bf052f9ab79f37b503e719423ab477
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-15 17:03:00 -0300

    feat: connect mysql service to api

--- api/app.py
+++ api/app.py
@@ -1,6 +1,15 @@
 from apiflask import APIFlask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = APIFlask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:password@localhost/db"
+db.init_app(app)
 
 @app.get('/')
 def home():
