0abf6a82da38bdcacfde3e31d410d89f0dc4e7a7
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-16 14:56:38 -0300

    feat: add void home page

---
+++ app-bloxs/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/home.module.css";
+import { loginRequest } from '@/api/index';
+
+export default function Home() {
+
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  )
+}
