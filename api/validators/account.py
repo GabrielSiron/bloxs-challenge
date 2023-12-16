fd7a9ce7826363766bb3da873812397ad847fc9b
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-16 11:11:33 -0300

    feat: add validator module for body requests

---
+++ api/validators/account.py
@@ -0,0 +1,16 @@
+from apiflask import Schema
+from apiflask.fields import String, DateTime
+from apiflask.validators import Length
+
+
+class CreateAccountValidator(Schema):
+    email = String(required=True, validate=Length(0, 50))
+    password = String(required=True, validate=Length(0, 50))
+    name = String(required=True, validate=Length(0, 50))
+    document_number = String(required=True, validate=Length(0, 50))
+    birth_date = DateTime(required=True)
+    
+class ChangePasswordValidator(Schema):
+    email = String(required=True, validate=Length(0, 50))
+    current_password = String(required=True, validate=Length(0, 50))
+    new_password = String(required=True, validate=Length(0, 50))
