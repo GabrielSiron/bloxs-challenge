b47b6e089829d1fdde26e0b7d77fd99a86b2222d
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-18 17:39:10 -0300

    feat: implements authenticate check

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 
-from apiflask import APIFlask
+from apiflask import APIFlask, abort
 from flask_migrate import Migrate
 from flask_cors import CORS
 import sqlalchemy
@@ -10,6 +10,9 @@ from endpoints import account_routes
 from endpoints import transaction_routes
 from services import db
 from seeder import seed_database
+import jwt
+from utils import token_has_expired
+from datetime import datetime
 import models
 
 def create_app():
@@ -36,6 +39,31 @@ def create_app():
 
     app.register_blueprint(account_routes)
     app.register_blueprint(transaction_routes)
+
+    @app.before_request
+    def is_authenticated():
+        from flask import request
+
+        if request.method == 'OPTIONS':
+            return
+        
+        if request.path in ['/login', '/signup']:
+            return
+        
+        token = request.headers.get('authorization')
+
+        if token:
+            try:
+                payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
+                expiration_date = datetime.strptime(payload['expires_in'][:19], '%Y-%m-%d %H:%M:%S')
+                if token_has_expired(expiration_date):
+                    abort(403, 'Token has expired. Please log in')
+
+            except Exception as e:
+                abort(403, str(e))
+        else:
+            
+            abort(403, 'Token is missing')
         
     return app
 
