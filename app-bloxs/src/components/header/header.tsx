e73bbf03ff565b2028911590762eca498003c118
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-19 17:33:21 -0300

    style: add box shadow to header

--- app-bloxs/src/components/header/header.tsx
+++ app-bloxs/src/components/header/header.tsx
@@ -6,7 +6,7 @@ import BloxsLogo from '../../assets/img/bloxs-logo.png'
 
 export default function Header(props: any){
     
-    const tabs = ['/home', '/account', '/transactions', 'deposit', 'withdrawal', '/pix']
+    const tabs = ['/about', '/account', '/transactions', 'deposit', 'withdrawal', '/pix']
     const router = useRouter()
     const redirect = (index: number) => {
         router.push(tabs[index])
@@ -18,40 +18,40 @@ export default function Header(props: any){
                 <h2 className={styles.enterpriseName}>Desafio  <Image src={BloxsLogo} alt='logo' width='50'></Image></h2>
             </div>
             <div className={styles.tabContainer}>
-                <Button onClick={() => {redirect(0)}} className={props.tabIndex == '0'? styles.selected : styles.notSelected}>Home</Button>
-                {
+                <Button onClick={() => {redirect(0)}} className={props.tabIndex == '0'? styles.selected : styles.notSelected}>Quem Somos</Button>
+                {/* {
                     props.tabIndex == '0' && <div className={styles.circle}></div>
-                }
+                } */}
             </div>
             <div className={styles.tabContainer}>
                 <Button onClick={() => {redirect(1)}} className={props.tabIndex == '1'? styles.selected : styles.notSelected}>Minha Conta</Button>
-                {
+                {/* {
                     props.tabIndex == '1' && <div className={styles.circle}></div>
-                }
+                } */}
             </div>
             <div className={styles.tabContainer}>
                 <Button onClick={() => {redirect(2)}} className={props.tabIndex == '2'? styles.selected : styles.notSelected}>Transações</Button>
-                {
+                {/* {
                     props.tabIndex == '2' && <div className={styles.circle}></div>
-                }
+                } */}
             </div>
             <div className={styles.tabContainer}>
                 <Button onClick={() => {redirect(3)}} className={props.tabIndex == '3'? styles.selected : styles.notSelected}>Depósito</Button>
-                {
+                {/* {
                     props.tabIndex == '3' && <div className={styles.circle}></div>
-                }
+                } */}
             </div>
             <div className={styles.tabContainer}>
                 <Button onClick={() => {redirect(4)}} className={props.tabIndex == '4'? styles.selected : styles.notSelected}>Saque</Button>
-                {
+                {/* {
                     props.tabIndex == '4' && <div className={styles.circle}></div>
-                }
+                } */}
             </div>
             <div className={styles.tabContainer}>
                 <Button onClick={() => {redirect(5)}} className={props.tabIndex == '5'? styles.selected : styles.notSelected}>Pix</Button>
-                {
+                {/* {
                     props.tabIndex == '5' && <div className={styles.circle}></div>
-                }
+                } */}
             </div>
         </div>
         )
