a3f456512d15c2ae193bf4554909da46b3053828
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-21 11:46:44 -0300

    formattig with black

--- api/app.py
+++ api/app.py
@@ -20,22 +20,24 @@ def create_app():
 
     migrate = Migrate()
 
-    testing = "Testando o limite de caracteres por linha definido no BLACK e testando se o pre-commit ira funcioncar corretamente"
-    driver = os.environ['DB_DRIVER']
-    user = os.environ['DB_USER']
-    password = os.environ['DB_PASSWORD']
-    host = os.environ['DB_HOST']
-    port = os.environ['DB_PORT']
-    db_name = os.environ['DB_NAME']
+    testing = "Testando o limite de caracteres por linha definido no BLACK e testando se o pre-commit ira funcioncar corretamente."
+    driver = os.environ["DB_DRIVER"]
+    user = os.environ["DB_USER"]
+    password = os.environ["DB_PASSWORD"]
+    host = os.environ["DB_HOST"]
+    port = os.environ["DB_PORT"]
+    db_name = os.environ["DB_NAME"]
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'{driver}://{user}:{password}@{host}:{port}/{db_name}'
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = f"{driver}://{user}:{password}@{host}:{port}/{db_name}"
 
-    cors = CORS(app, resources={r'/*': {'origins': '*'}})
-    
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
     with app.app_context():
         db.init_app(app)
         migrate.init_app(app, db)
-        db.create_all() 
+        db.create_all()
         seed_database()
 
     app.register_blueprint(account_routes)
@@ -45,33 +47,36 @@ def create_app():
     def is_authenticated():
         from flask import request
 
-        if request.method == 'OPTIONS':
+        if request.method == "OPTIONS":
             return
-        
-        if request.path in ['/login', '/signup']:
+
+        if request.path in ["/login", "/signup"]:
             return
-        
-        token = request.headers.get('Authorization')
+
+        token = request.headers.get("Authorization")
 
         if token:
             try:
-                payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
-                expiration_date = datetime.strptime(payload['expires_in'][:19], '%Y-%m-%d %H:%M:%S')
+                payload = jwt.decode(
+                    token, os.getenv("SECRET_KEY"), algorithms=["HS256"]
+                )
+                expiration_date = datetime.strptime(
+                    payload["expires_in"][:19], "%Y-%m-%d %H:%M:%S"
+                )
                 if token_has_expired(expiration_date):
-                    abort(403, 'Token has expired. Please log in')
+                    abort(403, "Token has expired. Please log in")
 
                 http_args = request.args.to_dict()
-                http_args['account_id'] = payload['account_id']
+                http_args["account_id"] = payload["account_id"]
 
                 request.args = ImmutableMultiDict(http_args)
             except Exception as e:
                 abort(403, str(e))
         else:
-            
-            abort(403, 'Token is missing')
-        
+            abort(403, "Token is missing")
+
     return app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
