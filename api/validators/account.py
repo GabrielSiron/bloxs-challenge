b75dd290e94d564df61c25aec8c3f070de2cbab8
Gabriel Siron <gabrielsiron.menezes@gmail.com>
2023-12-20 23:19:36 -0300

    refactor: adjust validators to transaction and account endpoints

--- api/validators/account.py
+++ api/validators/account.py
@@ -4,20 +4,20 @@ from apiflask.validators import Length
 
 
 class CreateAccountValidator(Schema):
-    email = String(required=True, validate=Length(0, 50))
-    password = String(required=True, validate=Length(0, 50))
-    name = String(required=True, validate=Length(0, 50))
-    document_number = String(required=True, validate=Length(0, 50))
+    email = String(required=True, validate=Length(10, 50))
+    password = String(required=True, validate=Length(8, 50))
+    name = String(required=True, validate=Length(3, 50))
+    document_number = String(required=True, validate=Length(14, 14))
     birth_date = DateTime(required=True)
 
 class ChangePasswordValidator(Schema):
     email = String(required=True, validate=Length(0, 50))
-    current_password = String(required=True, validate=Length(0, 50))
-    new_password = String(required=True, validate=Length(0, 50))
+    current_password = String(required=True, validate=Length(8, 50))
+    new_password = String(required=True, validate=Length(8, 50))
 
 class LoginValidator(Schema):
-    email = String(required=True, validate=Length(0, 50))
-    password = String(required=True, validate=Length(0, 50))
+    email = String(required=True, validate=Length(10, 50))
+    password = String(required=True, validate=Length(8, 50))
 
 class GetAccountInfo(Schema):
-    email = String(required=True, validate=Length(0, 50))
+    email = String(required=True, validate=Length(10, 50))
